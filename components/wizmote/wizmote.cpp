a0b9a1a55c31de99f086064c4cd7a110cc540331
Austin Hodak <ahodak65@gmail.com>
2022-09-27 00:47:55 -0400

    Update wizmote.cpp

--- components/wizmote/wizmote.cpp
+++ components/wizmote/wizmote.cpp
@@ -4,9 +4,10 @@ namespace esphome {
 namespace wizmote {
 void WizMoteListener::on_esp_now_message(esp_now::ESPNowPacket packet) {
   WizMotePacket wizmote = WizMotePacket::build(packet);
-  if (wizmote.sequence == this->last_sequence_)
+  if (wizmote.sequence <= this->last_sequence_ && wizmote.bssid == last_bssid)
     return;
 
+  this->last_bssid = wizmote.bssid;
   this->last_sequence_ = wizmote.sequence;
   this->on_button_->trigger(wizmote);
 }
